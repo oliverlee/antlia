cbf1dd8f3f9ababd330b7127a8eb571021431d62
Oliver Lee <oliverzlee@gmail.com>
2017-08-29 14:28:37 +0200

    Use Savitzky-Golay to filter velocity

--- python/path.py
+++ python/path.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import scipy.integrate
-#import scipy.signal
+import scipy.signal
 import matplotlib.patches
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -29,6 +29,7 @@ def plot_velocity(r, velocity_window_size):
     #w0 = f0/(fs/2) # normalized frequency
     #b, a = scipy.signal.iirnotch(w0, Q)
     #vf2 = scipy.signal.filtfilt(b, a, v)
+    vf2 = scipy.signal.savgol_filter(v, window_length=111, polyorder=5, deriv=0)
 
     colors = sns.color_palette('husl', 8)
     fig, axes = plt.subplots(2, 1)
@@ -39,9 +40,8 @@ def plot_velocity(r, velocity_window_size):
     ax.plot(t, vf, color=colors[1],
             label='velocity, gaussian weighted moving average, {} samples'.format(
                 velocity_window_size))
-    #ax.plot(t, vf2, color=colors[3],
-    #        label='velocity, notch filter, f0: {} Hz, Q: {}'.format(
-    #            f0, Q))
+    ax.plot(t, vf2, color=colors[3],
+            label='velocity, savgol filter (111, 5, 0)')
     ax.set_xlabel('time [s]')
     ax.set_ylabel('velocity [m/s]')
     ax.axhline(0, color=charcoal_color, linewidth=1, zorder=1)
@@ -49,14 +49,14 @@ def plot_velocity(r, velocity_window_size):
 
     freq, xf = ff.fft(v, dt) # uses hamming window
     freq, xf1 = ff.fft(vf, dt) # uses hamming window
-    #freq, xf2 = ff.fft(vf2, dt) # uses hamming window
+    freq, xf2 = ff.fft(vf2, dt) # uses hamming window
 
     ax = axes[1]
     #markerline, stemline, baseline = ax.stem(freq, xf, markerfmt=' ')
     #plt.setp(stemline, 'color', colors[1], 'alpha', 0.3)
     ax.plot(freq, xf, color=colors[0], alpha=0.3)
     ax.plot(freq, xf1, color=colors[1])
-    #ax.plot(freq, xf2, color=colors[3])
+    ax.plot(freq, xf2, color=colors[3])
     ax.set_yscale('log')
     ax.set_xlabel('frequency [Hz]')
     ax.set_ylabel('amplitude')
--- python/plot.py
+++ python/plot.py
@@ -135,12 +135,12 @@ if __name__ == '__main__':
     from matplotlib.backends.backend_pdf import PdfPages
     #pp = PdfPages('braking_plots.pdf')
     #pp = PdfPages('steering_plots.pdf')
-    pp = PdfPages('path_plots.pdf')
+    #pp = PdfPages('path_plots.pdf')
 
-    def save_fig(fig):
-        fig.set_size_inches(12.76, 7.19)
-        fig.tight_layout()
-        pp.savefig(fig)
+    #def save_fig(fig):
+    #    fig.set_size_inches(12.76, 7.19)
+    #    fig.tight_layout()
+    #    pp.savefig(fig)
 
     import record
     import pickle
@@ -195,22 +195,27 @@ if __name__ == '__main__':
 
     ## trajectory plots
     for rid, tid, r in recs:
-        if tid == 3 or tid == 4:
-            #path.plot_velocity(r, 55)
+        if rid > 1:
+            break
+        #if tid == 3 or tid == 4:
+        if tid == 4:
+            path.plot_velocity(r, 55)
 
-            soln, fig, axes, fig2, ax2 = path.get_trajectory(
-                    r,
-                    velocity_window_size=55,
-                    yaw_rate_window_size=11,
-                    plot=True,
-                    trial_id=tid)
-            fig.suptitle('filtered signals rider {} trial {}'.format(
-                rid, tid))
-            save_fig(fig)
+            #plot_timeseries(r)
 
-            fig2.suptitle('trajectory rider {} trial {}'.format(rid, tid))
-            save_fig(fig2)
-            print('generated trajectory {} {}'.format(rid, tid))
+            #soln, fig, axes, fig2, ax2 = path.get_trajectory(
+            #        r,
+            #        velocity_window_size=55,
+            #        yaw_rate_window_size=11,
+            #        plot=True,
+            #        trial_id=tid)
+            #fig.suptitle('filtered signals rider {} trial {}'.format(
+            #    rid, tid))
+            #save_fig(fig)
 
-    #plt.show()
-    pp.close()
+            #fig2.suptitle('trajectory rider {} trial {}'.format(rid, tid))
+            #save_fig(fig2)
+            #print('generated trajectory {} {}'.format(rid, tid))
+
+    plt.show()
+    #pp.close()
