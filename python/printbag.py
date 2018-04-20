b583c5fb00d1ebfa0458a6233be85d8b56173abf
Oliver Lee <oliverzlee@gmail.com>
2018-04-20 15:07:32 +0200

    Add argument to specify bag topics

--- python/printbag.py
+++ python/printbag.py
@@ -2,38 +2,36 @@
 # -*- coding: utf-8 -*-
 """Print a rosbag file.
 """
-
 import sys
 import logging
-
-import numpy as np
 
 # suppress logging warnings due to rospy
 logging.basicConfig(filename='/dev/null')
 import rosbag
 
-from antlia.dtype import LIDAR_CONVERTED_DTYPE
 
 def print_bag(bag, topics=None):
-    if topics is None:
-        #topics = ['/tf', '/scan']
-        topics = ['/scan', '/flagbutton_pressed']
-
     for message in bag.read_messages(topics=topics):
         print(message)
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print(('Usage: {} <rosbag> \n\n'
-               'Print contents of rosbag file.'
+        print(('Usage: {} [topics] <rosbag> \n\n'
+               'topics:\tcomma-separated list of topics\n\n'
+               'Print contents of rosbag file. If topics is not provided, \n'
+               'all topics are printed\n'
                ).format(__file__))
         sys.exit(1)
 
-    outfile = None
-    filename = sys.argv[1]
+    topics = None
+    if len(sys.argv) == 3:
+        topics = [t.strip() for t in sys.argv[1].split(',')]
+        filename = sys.argv[2]
+    else:
+        filename = sys.argv[1]
 
     with rosbag.Bag(filename) as bag:
-        print_bag(bag)
+        print_bag(bag, topics)
 
     sys.exit()
