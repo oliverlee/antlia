09c7b2c5bffbce58fd18bf0fd5117c9b6402dfa6
Oliver Lee <oliverzlee@gmail.com>
2018-05-17 14:51:08 +0200

    Update event detection using entry/exit bounding boxes

--- python/antlia/trial2.py
+++ python/antlia/trial2.py
@@ -14,10 +14,12 @@ class EventType(Enum):
     def __str__(self):
         return self.name
 
-EventClassificationData = namedtuple('EventClassificationData',
-                                     ['v0', 'v1', 'vratio'])
-EventDetectionData = namedtuple('EventDetectionData',
-                                ['mask_a', 'mask_b'])
+EventClassificationData = namedtuple(
+        'EventClassificationData',
+        ['v0', 'v1', 'vratio'])
+EventDetectionData = namedtuple(
+        'EventDetectionData',
+        ['mask_a', 'mask_b', 'mask_event', 'entry_time', 'exit_time'])
 
 class Trial2(Trial):
     def __init__(self, bicycle_data, lidar_data, period, lidar_bbmask=None):
@@ -27,18 +29,23 @@ class Trial2(Trial):
 
             # self.event_indices
             # self.event_timerange
+            # self.event_detection
             self._detect_event(bbmask_kw=lidar_bbmask)
 
             # self.event_type
-            # self.event_classification_data
+            # self.event_classification
             self._classify_event()
 
     def _detect_event(self, bbmask_kw=None):
         """Event is detected using two masks, one on the bicycle speed sensor
         and one on the lidar data. Mask A detects when the bicycle speed is
-        greater than 1. Mask B detects if any object is visible to the lidar,
+        greater than 0.5. Mask B detects if any object is visible to the lidar,
         within the bounding box specified by (0, 0) and (60, 4) with respect to
         the lidar reference frame.
+
+        This region is then further reduced by detecting the cyclist appearing
+        in the bounding box (30, 2), (50, 4) and (possibly, in the case of
+        overtaking) and in the bounding box (-20, 2), (-10, 4).
 
         Parameters:
         bbmask_kw: dict, keywords supplied to lidar.cartesian() for an area to
@@ -77,12 +84,56 @@ class Trial2(Trial):
                     (np.mean(self.bicycle.speed[e[0]:e[1]]) > MIN_AVG_SPEED))]
 
         assert len(evti) > 0, "unable to detect event for this trial"
+        event = evti[-1]
 
-        self.event_detection_data = EventDetectionData(
-                mask_a, mask_b)
-        self.event_indices = evti[-1]
-        self.event_timerange = (self.bicycle.time[self.event_indices[0]],
-                                self.bicycle.time[self.event_indices[1]])
+        # reduce region using entry and exit bounding box detection
+        entry_mask = self.lidar.cartesian(
+                        xlim=(30, 50),
+                        ylim=(2, 4))[0].count(axis=1) > 1
+        exit_mask = self.lidar.cartesian(
+                        xlim=(-20, -10),
+                        ylim=(2, 4))[0].count(axis=1) > 1
+
+        # find time where cyclist enters lidar vision
+        entry_time = None
+        for x in np.where(entry_mask > 0)[0]:
+            t = self.lidar.time[x]
+            if (t >= self.bicycle.time[event[0]] and
+                t < self.bicycle.time[event[1]]):
+                entry_time = t
+                try:
+                    i = np.where(self.bicycle.time >= t)[0][0]
+                except IndexError:
+                    # error going from lidar time to bicycle time
+                    msg = 'Unable to detect cyclist entry for trial. '
+                    msg += 'Event detection failure.'
+                    raise IndexError(msg)
+                event = (i, event[1])
+                break
+        assert entry_time is not None, 'No cyclist entry detected'
+
+        # find time where cyclist has finished overtaking obstacle, if it exists
+        exit_time = None
+        for x in np.where(exit_mask > 0)[0][::-1]:
+            t = self.lidar.time[x]
+            if (t >= self.bicycle.time[event[0]] and
+                t < self.bicycle.time[event[1]]):
+                exit_time = t
+                try:
+                    i = np.where(self.bicycle.time > t)[0][0]
+                except IndexError:
+                    # error going from lidar time to bicycle time
+                    msg = 'Unable to detect cyclist exit for trial. '
+                    msg += 'Event detection failure.'
+                    raise IndexError(msg)
+                event = (event[0], i - 1)
+                break
+
+        self.event_detection = EventDetectionData(
+                mask_a, mask_b, evti[-1], entry_time, exit_time)
+        self.event_indices = event
+        self.event_timerange = (self.bicycle.time[event[0]],
+                                self.bicycle.time[event[1]])
 
     def _classify_event(self):
         """Classifies an event as braking or overtaking maneuver.
@@ -91,7 +142,6 @@ class Trial2(Trial):
         event start and end.
         """
         BRAKING_VELOCITY_THRESHOLD = 0.80
-
 
         # Compare first eighth and last eighth
         i0, i1 = self.event_indices
@@ -103,10 +153,10 @@ class Trial2(Trial):
         v0 = np.mean(self.bicycle.speed[a0:a1])
         v1 = np.mean(self.bicycle.speed[b0:b1])
         vratio = v1/v0
-        self.event_classification_data = EventClassificationData(v0, v1, vratio)
+        self.event_classification = EventClassificationData(v0, v1, vratio)
 
-        if vratio < BRAKING_VELOCITY_THRESHOLD:
+        #if vratio < BRAKING_VELOCITY_THRESHOLD:
+        if self.event_detection.exit_time is None:
             self.event_type = EventType.Braking
         else:
             self.event_type = EventType.Overtaking
-
